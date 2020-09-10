ace6e3e9b8637e12a960a975737330a312066b1f
xipfs <flexie@foxmail.com>
2020-09-10 18:04:50 +0800

    first commit

---
+++ src/main/java/tinyipfs/IPFS.java
@@ -0,0 +1,14 @@
+package tinyipfs;
+
+/**
+ * description: IPFS <br>
+ *
+ * @author xie hui <br>
+ * @version 1.0 <br>
+ * @date 2020/9/10 18:03 <br>
+ */
+public class IPFS {
+    public static void main(String[] args){
+        System.out.println("tiny ipfs");
+    }
+}
